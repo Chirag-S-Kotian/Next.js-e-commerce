b22608cdae60ee618eed129b68c61d9d66935259
Chirag-S-kotian <ckotian117@gmail.com>
2024-05-11 20:18:08 +0530

    Update homepage with new header and description
    - Replace small logo image with a larger version
    - Add header with "Your Image on a Custom Phone Case"
    - Add description "Capture your favourite memories with your own, one-on

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,19 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <img src="/snake-1.png" alt="logo" className="" />
+                <img src="/snake-1.png" alt="logo" className="w-full" />
               </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image on a{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>
+                Phone Case
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture your favourite memories with your own,{" "}
+                <span className="font-semibold">one-on-one</span>phone case.
+                Casecobra allows you to protect your memories not just your
+                phone case.
+              </p>
             </div>
           </div>
         </MaxWidthWrapper>
