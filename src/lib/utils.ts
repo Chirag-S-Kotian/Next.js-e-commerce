9a2b30844bcbd9637a181945f9bb8e6116bbf3a6
Chirag-S-kotian <ckotian117@gmail.com>
2024-05-13 20:24:06 +0530

    Update metadata in constructMetadata function
    * Change creator from "@joshtriedcoding" to "@CHIRAG"
    * Update metadataBase URL from "<https://casecobra.vercel.app/>" to "<https://

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -39,9 +39,9 @@ export function constructMetadata({
       title,
       description,
       images: [image],
-      creator: "@joshtriedcoding",
+      creator: "@CHIRAG",
     },
     icons,
-    metadataBase: new URL("https://casecobra.vercel.app/"),
+    metadataBase: new URL("https://https://next-e1-commerce.vercel.app/"),
   };
 }
